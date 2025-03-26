c845e34979c43344e8e1b52385725f233bb5d845
miles-lenz <miles@lenz-be.de>
2025-03-26 16:22:55 +0100

    add prompt to adjust the chatbot behaviour

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from ollama import chat
 import gradio as gr
 
 
+SYSTEM_PROMPT = open("prompt.txt", encoding="utf-8").read().strip()
+
 # Specify the model used for the chatbot.
 # Ensure that the model is compatible with Ollama and is downloaded
 # locally using `ollama pull <model_name>`.
@@ -28,7 +30,11 @@ def get_response(message: str, history: list[dict]) -> Generator:
         str: The progressively generated chatbot response.
     """
 
-    messages = history + [{"role": "user", "content": message}]
+    messages = [
+        {"role": "system", "content": SYSTEM_PROMPT},
+        *history,
+        {"role": "user", "content": message}
+    ]
 
     stream = chat(model=MODEL, messages=messages, stream=True)
 
@@ -38,9 +44,24 @@ def get_response(message: str, history: list[dict]) -> Generator:
         yield response
 
 
+# Create an initial welcome message by generating the response with no input
+# or history and just the system prompt.
+init_message = ""
+for chunk in get_response("", []):
+    init_message = chunk
+
+# Create a custom gradio chatbot with the initial messages.
+chatbot = gr.Chatbot(
+    type="messages",
+    scale=1,
+    height=400,
+    value=[{"role": "assistant", "content": init_message}]
+)
+
 # Create a Gradio chat interface to allow interaction with the chatbot.
 demo = gr.ChatInterface(
     fn=get_response,
+    chatbot=chatbot,
     type="messages",
     title="Travel Chatbot",
 )
